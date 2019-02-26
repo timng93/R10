4f65b196a2dc6a3feb814a62469ff98c7ed12473
Timmy <timgabrielnguyen@gmail.com>
2019-02-25 17:40:24 -0800

    Displayed Items on About Page

--- js/screens/About/About.js
+++ js/screens/About/About.js
@@ -1,18 +1,31 @@
 import React, { Component } from "react";
-import { Text, View, Image, ScrollView } from "react-native";
+import { Text, View, Image, ScrollView, FlatList } from "react-native";
 import styles from "./styles";
 class About extends Component {
   constructor(props) {
     super(props);
-    
   }
 
   render() {
-      console.log(this.props);
+    console.log(this.props.data.allConducts);
     return (
-      <ScrollView style={styles.container}>
-     
-      </ScrollView>
+      <View style={{ background: "red" }}>
+        <FlatList
+          style={styles.list}
+          data={this.props.data.allConducts}
+          renderItem={({ item }) => (
+            <View style={{ flexDirection: "row" }}>
+              <Text style={{ color: "white" }} >{item.title}</Text>
+            </View>
+          )}
+          ItemSeparatorComponent={() => (
+            <View style={{ borderStyle: "solid", borderWidth: 0.5 }} />
+          )}
+          keyExtractor={item => item.id + ""}
+          // ItemSeparatorComponent={() => <Text>I am a separator</Text>}
+        //   keyExtractor={item => item.index + ""}
+        />
+      </View>
     );
   }
 }
---
+++ js/screens/About/styles.js
@@ -0,0 +1,17 @@
+import {StyleSheet, Dimensions} from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#F5FCFF"
+    },
+    
+    list: {
+        color: "#F5FCFF"
+        // flex: 1
+    }
+    
+  });
+  
+
+  export default styles;
