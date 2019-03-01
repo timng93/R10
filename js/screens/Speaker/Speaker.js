a4f36a702cf1f7a4e16fad0559884eb669f925d7
Timmy <timgabrielnguyen@gmail.com>
2019-03-01 00:06:13 -0800

    Centered Contents in Speaker

--- js/screens/Speaker/Speaker.js
+++ js/screens/Speaker/Speaker.js
@@ -5,23 +5,31 @@ const Speaker = props => {
   console.log(props.speaker);
   return (
     <ScrollView style={styles.container}>
-
-       <View style={styles.background}>
-       <Button style={styles.navigationButton} onPress={() => props.navigation.goBack()} title="Back Button" />
-
-      <Button style={styles.navigationButton} onPress={() => props.navigation.goBack()} title="Back Button" />
-      <View style={styles.speaker}>
-        <Image style={styles.image} source={{ uri: props.speaker.image }} />
-        <Text style={styles.title}>{props.speaker.name}</Text>
-        <Text style={styles.description}>{props.speaker.bio}</Text>
-
+      <View style={styles.background}>
         <Button
-          onPress={() => Linking.openURL(`${props.speaker.url}`)}
-          title="Learn More on Wikipedia"
-          color="#841584"
-          accessibilityLabel="Learn more about this speaker on Wikipedia"
+          style={styles.navigationButton}
+          onPress={() => props.navigation.goBack()}
+          title=""
         />
-      </View>
+        <Button
+          style={styles.navigationButton}
+          onPress={() => props.navigation.goBack()}
+          title="X"
+        />
+        <View style={styles.speaker}>
+          <View style={styles.speakerContainer}>
+            <Image style={styles.image} source={{ uri: props.speaker.image }} />
+            <Text style={styles.title}>{props.speaker.name}</Text>
+          </View>
+          <Text style={styles.description}>{props.speaker.bio}</Text>
+
+          <Button
+            onPress={() => Linking.openURL(`${props.speaker.url}`)}
+            title="Learn More on Wikipedia"
+            color="#841584"
+            accessibilityLabel="Learn more about this speaker on Wikipedia"
+          />
+        </View>
       </View>
     </ScrollView>
   );
--- js/screens/Speaker/styles.js
+++ js/screens/Speaker/styles.js
@@ -4,30 +4,31 @@ const styles = StyleSheet.create({
   background: {
     backgroundColor: "#000"
   },
-//   navigationButton: {
-//     marginTop: 25
-//   },
+  navigationButton: {
+    marginTop: 55
+  },
   speaker: {
     backgroundColor: "#fff",
     marginTop: 40
   },
+
   image: {
     height: 120,
     width: 120,
     borderRadius: 55,
-    marginLeft: 130,
+    left: "35%",
     marginTop: 30
   },
   title: {
     fontSize: 30,
     paddingTop: 15,
     fontWeight: "600",
-    marginLeft: 100
+    left: "27%"
   },
   description: {
     fontSize: 18,
-    marginLeft: 10,
-    paddingTop: 15
+    paddingTop: 15,
+    padding: 10
   }
 });
 
