e13726f52a6252be7ec89a294b364ed2c5a31c9b
Timmy <timgabrielnguyen@gmail.com>
2019-02-28 23:16:41 -0800

    Finalized Handler for Android

--- android/app/src/main/java/com/projectr10/MainActivity.java
+++ android/app/src/main/java/com/projectr10/MainActivity.java
@@ -1,8 +1,13 @@
 package com.projectr10;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
+
+
 
     /**
      * Returns the name of the main component registered from JavaScript.
@@ -12,4 +17,14 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "ProjectR10";
     }
+
+      @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+       return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
+  }
 }
